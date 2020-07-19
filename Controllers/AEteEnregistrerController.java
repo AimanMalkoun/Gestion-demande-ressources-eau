80feb549fffddee849d0bc14a21527b98c6836d1
mohamed matrouh <mohamed.matrouh16@gmail.com>
2020-07-19 01:14:05 +0100

    merin with hasanBanch

--- Controllers/AEteEnregistrerController.java
+++ Controllers/AEteEnregistrerController.java
@@ -82,12 +82,12 @@ public class AEteEnregistrerController implements Initializable {
 			/* les elements du paragraphe 1 */
 
 			Paragraph para1 = new Paragraph(30);
-			para1.add(new Phrase("\n Ø§Ù„Ù…Ù…Ù„ÙƒØ© Ø§Ù„Ù…ØºØ±Ø¨ÙŠØ© \n", big0));// ÙˆØ²Ø§Ø±Ø© Ø§Ù„ØªØ¬Ù‡ÙŠØ² ÙˆØ§Ù„Ù†Ù‚Ù„ ÙˆØ§Ù„Ù„ÙˆØ¬ÙŠØ³ØªÙŠÙƒ ÙˆØ§Ù„Ù…Ø§Ø¡ ",
+			para1.add(new Phrase("\n  \n", big0));// ÙˆØ²Ø§Ø±Ø© Ø§Ù„ØªØ¬Ù‡ÙŠØ² ÙˆØ§Ù„Ù†Ù‚Ù„ ÙˆØ§Ù„Ù„ÙˆØ¬ÙŠØ³ØªÙŠÙƒ ÙˆØ§Ù„Ù…Ø§Ø¡ ",
 																	// normal));
-			para1.add(new Phrase("ÙˆØ²Ø§Ø±Ø© Ø§Ù„ØªØ¬Ù‡ÙŠØ² ÙˆØ§Ù„Ù†Ù‚Ù„ ÙˆØ§Ù„Ù„ÙˆØ¬ÙŠØ³ØªÙŠÙƒ ÙˆØ§Ù„Ù…Ø§Ø¡", big0));
-			para1.add(new Phrase("\n Ù‚Ø·Ø§Ø¹ Ø§Ù„Ù…Ø§Ø¡\n", big));
+			para1.add(new Phrase("", big0));
+			para1.add(new Phrase("\n\n", big));
 			para1.add(new Phrase(
-					"Ø§Ù„Ù…Ø¯Ø±ÙŠØ© Ø§Ù„Ø§Ù‚Ù„ÙŠÙ…ÙŠØ© Ù„Ù„ØªØ¬Ù‡ÙŠØ² ÙˆØ§Ù„Ù†Ù‚Ù„ ÙˆØ§Ù„Ù„ÙˆØ¬ÙŠØ³ØªÙŠÙƒ ÙˆØ§Ù„Ù…Ø§Ø¡ Ø¨Ø£Ø³Ù�ÙŠ \n Ø§Ù„Ù…ØµÙ„Ø­Ø© Ø§Ù„Ø§Ù‚Ù„ÙŠÙ…ÙŠØ© Ù„Ù„Ù…Ø§Ø¡ Ø¨Ø£Ø³Ù�ÙŠ \n",
+					" \n",
 					normal));
 			para1.add(new Phrase("\n ÙˆØµÙ„ Ø¥ÙŠØ¯Ø§Ø¹\n", big));
 			para1.setAlignment(Paragraph.ALIGN_CENTER);
@@ -95,40 +95,40 @@ public class AEteEnregistrerController implements Initializable {
 			/* les elements du paragraphe 2 */
 
 			Paragraph para2 = new Paragraph(30);// " \n ÙŠØ´Ù‡Ø¯ Ø±Ø¦ÙŠØ³ Ø§Ù„Ù…ØµÙ„Ø­Ø© Ø§Ù„Ø§Ù‚Ù„ÙŠÙ…ÙŠØ© Ù„Ù„Ù…Ø§Ø¡ Ø¨Ø£Ø³Ù�ÙŠ Ø§Ù† Ø§Ù„Ø³ÙŠØ¯", normal);
-			para2.add(new Phrase("\n ÙŠØ´Ù‡Ø¯ Ø±Ø¦ÙŠØ³ Ø§Ù„Ù…ØµÙ„Ø­Ø© Ø§Ù„Ø§Ù‚Ù„ÙŠÙ…ÙŠØ© Ù„Ù„Ù…Ø§Ø¡ Ø¨Ø£Ø³Ù�ÙŠ Ø§Ù† Ø§Ù„Ø³ÙŠØ¯ ", normal));
+			para2.add(new Phrase("\n  ", normal));
 			para2.add(new Phrase(nomPrenom, big));
-			para2.add(new Phrase(" Ø­Ø§Ù…Ù„ Ù„Ù„Ø¨Ø·Ø§Ù‚Ø© Ø§Ù„ÙˆØ·Ù†ÙŠØ© Ø±Ù‚Ù… ", normal));
+			para2.add(new Phrase("  ", normal));
 			para2.add(new Phrase(cin, big));
-			para2.add(new Phrase(" Ù‚Ø¯ ÙˆØ¶Ø¹  Ù„Ø°Ø§ Ù‡Ø°Ù‡ Ø§Ù„Ù…ØµÙ„Ø­Ø©  Ø¨ØªØ§Ø±ÙŠØ® ", normal));
+			para2.add(new Phrase("  ", normal));
 			para2.add(new Phrase(""+dateDepot, big));
-			para2.add(new Phrase(" Ù…Ù„Ù� Ø·Ù„Ø¨ Ø§Ù„ØªØ±Ø®ÙŠØµ Ù„Ø¥Ù†Ø¬Ø§Ø² Ø«Ù‚Ø¨  Ù…Ø§Ø¦ÙŠ Ù…Ù† Ø£Ø¬Ù„ Ø³Ù‚ÙŠ Ø¨Ø§Ù„Ø¹Ù‚Ø§Ø± Ø§Ù„Ù…Ø³Ù…Ù‰ : ", normal));
+			para2.add(new Phrase("  ", normal));
 			para2.add(new Phrase(nomImmobiler, big));
 			para2.setAlignment(Paragraph.ALIGN_LEFT);
 			para2.setSpacingAfter(30);
 			/* les elements du paragraphe 3 */
 
 			Paragraph para3 = new Paragraph(30);
-			para3.add(new Phrase("Ø§Ù„Ø¬Ù…Ø§Ø¹Ø©", normal));
+			para3.add(new Phrase("", normal));
 			para3.add(new Phrase(commune, big));
-			para3.add(new Phrase(" Ø§Ù„Ù‚ÙŠØ§Ø¯Ø© ", normal));
-			para3.add(new Phrase(" Ø§Ø³Ù… Ø§Ù„Ù‚ÙŠØ§Ø¯Ø©  ", big));
-			para3.add(new Phrase(" Ø§Ù„Ø¯Ø§Ø¦Ø±Ø© ", normal));
-			para3.add(new Phrase(" Ø§Ø³Ù… Ø§Ù„Ø¯Ø§Ø¦Ø±Ø©  ", big));
-			para3.add(new Phrase(" Ø§Ù„Ø§Ù‚Ù„ÙŠÙ… ", normal));
+			para3.add(new Phrase("  ", normal));
+			para3.add(new Phrase("   ", big));
+			para3.add(new Phrase("  ", normal));
+			para3.add(new Phrase("   ", big));
+			para3.add(new Phrase("  ", normal));
 			para3.add(new Phrase(prevince+ ".", big));
 			para3.setAlignment(Paragraph.ALIGN_LEFT);
 
 			/* les elements du paragraphe 4 */
 
-			Paragraph para4 = new Paragraph("\n  Ø¥Ù…Ø¶Ø§Ø¡ : \n", big);
+			Paragraph para4 = new Paragraph("\n  : \n", big);
 			para4.setAlignment(Paragraph.ALIGN_CENTER);
 			para4.setSpacingAfter(35);
 
 			/* les elements du paragraphe 5 */
 
 			Paragraph para5 = new Paragraph(30);
-			para5.add(new Phrase("Ù…Ù„Ø­ÙˆØ¸Ø© : ", big0));
-			para5.add(new Phrase("Ù‡Ø°Ø§ Ø§Ù„ÙˆØµÙ„ ÙŠØ«Ø¨Øª Ù�Ù‚Ø· Ø¥ÙŠØ¯Ø§Ø¹ Ù…Ù„Ù� Ø§Ù„ØªØ±Ø®ÙŠØµ  ÙˆÙ„Ø§ ÙŠÙ…ÙƒÙ† Ø§Ø¹ØªØ¨Ø§Ø±Ù‡ ØªØ±Ø®ÙŠØµØ§ Ø¨Ø­Ù�Ø± Ø§Ùˆ Ø¬Ù„Ø¨ Ù…Ø§Ø¡ .",
+			para5.add(new Phrase(" ", big0));
+			para5.add(new Phrase("",
 					small));
 			para5.setAlignment(Paragraph.ALIGN_LEFT);
 			para5.setSpacingAfter(50);
