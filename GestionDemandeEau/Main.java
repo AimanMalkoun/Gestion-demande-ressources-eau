7b758ead3223134eb94ff90fbcde240215b24828
Tazribine Hassangotg^^pkokfrefof,gfofzncgortgotgit <tazribinehassan1998@gmail.com>
2020-07-21 13:15:42 +0100

    added some css files

--- GestionDemandeEau/Main.java
+++ GestionDemandeEau/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
 			 * 
 			 */
 			Parent dashboardRoot = (Parent) FXMLLoader
-					.load(getClass().getResource("../Fxml/InformationsDuDemandeur.fxml"));
+					.load(getClass().getResource("../Fxml/InformationsConcernantPointDeau.fxml"));
 			Scene dashboardScene = new Scene(dashboardRoot);
 
 			primaryStage.setTitle("Gestion des demandes d'autorisation pour l'utilisation des ressources en eau.");
