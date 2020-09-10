7b74f2544d2b9c71a7f51244e95be77c0d480055
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2020-09-11 00:17:49 +0200

    --

--- Controllers/DisplayFolderController.java
+++ Controllers/DisplayFolderController.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.ResourceBundle;
 
-import org.jpedal.exception.PdfException;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -18,6 +17,7 @@ import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfException;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
